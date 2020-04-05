5ab362f8ef2e897d36e09ef19eca765b37f798f0
Jared Buckner <buckner.jared@gmail.com>
2020-04-04 21:38:25 -0600

    Update access secret request

--- infra/db/db.go
+++ infra/db/db.go
@@ -3,7 +3,6 @@ package db
 import (
 	"context"
 	"fmt"
-	"log"
 
 	secretmanager "cloud.google.com/go/secretmanager/apiv1"
 	secretmanagerpb "google.golang.org/genproto/googleapis/cloud/secretmanager/v1"
@@ -19,35 +18,65 @@ type Credentials struct {
 // DB returns a database connection
 func DB() {
 	fmt.Println("testing")
-	creds := getDBCredentials()
+	creds, _ := accessSecretVersion("projects/buddyfit/secrets/DB_USER/versions/latest")
 
 	fmt.Println("Creds: ", creds)
 }
 
-func getDBCredentials() Credentials {
-	// GCP project in which to store secrets in Secret Manager.
-	projectID := "buddyfit"
+// accessSecretVersion accesses the payload for the given secret version if one
+// exists. The version can be a version number as a string (e.g. "5") or an
+// alias (e.g. "latest").
+func accessSecretVersion(name string) ([]byte, error) {
+	// name := "projects/my-project/secrets/my-secret/versions/5"
+	// name := "projects/my-project/secrets/my-secret/versions/latest"
 
 	// Create the client.
 	ctx := context.Background()
 	client, err := secretmanager.NewClient(ctx)
 	if err != nil {
-		log.Fatalf("failed to setup client: %v", err)
+		return nil, fmt.Errorf("failed to create secretmanager client: %v", err)
 	}
 
-	secretKey := "DB_USER"
-
-	// Create the request to create the secret.
-	getSecretReq := &secretmanagerpb.GetSecretRequest{
-		Name: fmt.Sprintf("projects/%s/secrets/%s", projectID, secretKey),
+	// Build the request.
+	req := &secretmanagerpb.AccessSecretVersionRequest{
+		Name: name,
 	}
 
-	secret, err := client.GetSecret(ctx, getSecretReq)
+	// Call the API.
+	result, err := client.AccessSecretVersion(ctx, req)
 	if err != nil {
-		log.Fatalf("failed to get secret: %v", err)
+		return nil, fmt.Errorf("failed to access secret version: %v", err)
 	}
 
-	fmt.Println("Got secret: ", secret.Name)
-
-	return Credentials{}
+	// WARNING: Do not print the secret in a production environment - this snippet
+	// is showing how to access the secret material.
+	return nil, result.Payload.Data
 }
+
+// func getDBCredentials() Credentials {
+// 	// GCP project in which to store secrets in Secret Manager.
+// 	projectID := "buddyfit"
+
+// 	// Create the client.
+// 	ctx := context.Background()
+// 	client, err := secretmanager.NewClient(ctx)
+// 	if err != nil {
+// 		log.Fatalf("failed to setup client: %v", err)
+// 	}
+
+// 	secretKey := "DB_USER"
+
+// 	// Create the request to create the secret.
+// 	getSecretReq := &secretmanagerpb.AccessSecretVersionRequest{
+// 		Name: fmt.Sprintf("projects/%s/secrets/%s/versions/latest", projectID, secretKey),
+// 	}
+
+// 	secret, err := client.GetSecret(ctx, getSecretVersionReq)
+// 	if err != nil {
+// 		log.Fatalf("failed to get secret: %v", err)
+// 	}
+
+// 	fmt.Println("Got secret: ", secret.Name)
+
+// 	return Credentials{}
+// }
